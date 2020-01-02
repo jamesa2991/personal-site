7ebb5122d6d7fe12cc446c9adaf7c7893ba8aa68
Jay <jay.allen@actual-experience.com>
2020-01-02 13:12:04 +0000

    [social-icons] Added icons and styled them

--- src/js/components/main.js
+++ src/js/components/main.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import propTypes from 'prop-types';
-import PersonalInfo from '~/components/personal-info';
+import ImageAndSocials from '~/components/image-and-socials';
 
 export default function Main(props) {
   return (
     <div className="content">
       <div className="main-info">
-        <PersonalInfo />
+        <ImageAndSocials />
       </div>
       <div className="main-content">
         <p> Welcome! </p>
--- webpack.config.js
+++ webpack.config.js
@@ -36,8 +36,12 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
+        test: /\.(png|jpg|gif)$/,
         use: ['file-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
       },
     ],
   },
